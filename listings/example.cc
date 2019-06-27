6c868c4a09cb18c4c8aacd4b8f321d3ad90975aa
Ivar Farup <ivar.farup@ntnu.no>
2019-06-27 14:25:14 +0200

    added project from overleaf

---
+++ listings/example.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+// No comment...
+int main() 
+{
+  cout << "Hello, World!" << endl;
+  return 0;
+}
---
+++ listings/example.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Comment
+x = np.linspace(0, 1)
+y = np.sin(2 * np.pi * x)
+
+plt.plot(x, y)
+plt.show()
